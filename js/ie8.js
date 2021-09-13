fea199dbb9272f401cfb9e4060bd88ec96c0385e
Nel-Kevin Junior Dubois <67355423+nelkevinjunior@users.noreply.github.com>
2021-09-13 17:47:35 -0400

    Add files via upload

---
+++ js/ie8.js
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2020 KVN Top Model
+ * Author   : Kvn Teck
+ * Website  : https://kvntechnology.com
+*/
+
+jQuery(document).ready(function(e) {
+    jQuery('body').html('<div class="ie8"><span>Hi! Please, use the latest version of the browser in order to visit current page</span></div>')
+});
